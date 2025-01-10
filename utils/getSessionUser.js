609bb9bde07e97d14aa08ebc71bda05fc3b03518
jason <jason@herman-scheer.com>
2025-01-10 09:43:51 -1000

    updating next-auth to v4

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -1,11 +1,8 @@
-//import { getServerSession } from 'next-auth'
 import { getServerSession } from 'next-auth/next'
-//import { auth } from '@/auth'
 import { authOptions } from '@/utils/authOptions'
 
 export const getSessionUser = async () => {
   const session = await getServerSession(authOptions)
-  //const session = await auth(authOptions)
 
   if (!session || !session.user) {
     return null
